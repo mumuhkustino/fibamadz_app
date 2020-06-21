91bd1abc1d7ffe8e7347cd6ecdb83aa907402745
nexain <ahmadajinaufalali@gmail.com>
2020-06-21 17:43:05 +0700

    tambah menu materi

---
+++ lib/menu_materi_jenazah.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MenuMateriJenazah extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
