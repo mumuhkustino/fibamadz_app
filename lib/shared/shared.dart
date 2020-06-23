e6487ad50b925fa8207a93a9ce253a6bd230fe0d
mumuhkustino <mumuhkustino@gmail.com>
2020-06-23 18:27:36 +0700

    Add const color, text style, and prevPageEvent

---
+++ lib/shared/shared.dart
@@ -0,0 +1,21 @@
+import 'package:fibamadz_app/bloc/blocs.dart';
+import 'package:flutter/material.dart';
+
+PageEvent prevPageEvent;
+
+const double defaultMargin = 24;
+
+Color colorYellow = Color(0xFFF9C22E);
+Color colorPink = Color(0xFFE01A4F);
+Color colorLightBlue = Color(0xFF56CCF2);
+Color colorDarkGrey = Color(0xFF4F4F4F);
+Color colorCream = Color(0x1AF2C94C);
+
+Color colorTextWhite = Color(0xFFFFFBFB);
+Color colorTextBlack = Color(0xFF000000);
+
+TextStyle whiteTextFont = TextStyle(
+    color: colorTextWhite, fontFamily: "Raleway", fontWeight: FontWeight.w500);
+
+TextStyle blackTextFont = TextStyle(
+    color: colorTextBlack, fontFamily: "Raleway", fontWeight: FontWeight.w500);
