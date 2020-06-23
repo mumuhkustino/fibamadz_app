c9dc035ddb2ff298e261181184ecdafdbe9e91ef
mumuhkustino <mumuhkustino@gmail.com>
2020-06-23 18:28:48 +0700

    Add multi bloc provider and bloc builder in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:fibamadz_app/splash.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/blocs.dart';
+import 'ui/pages/pages.dart';
 
 //method ini adalah method main yang dijalankan saat app pertamakali dibuka
 //method ini akan mengembalikan class MyApp
@@ -7,14 +10,23 @@ void main() {
   runApp(MyApp());
 }
 
-
 // class MyApp adalah stateless widget, yaitu widget yang tidak memerlukan
-// interaksi apapun 
+// interaksi apapun
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Splash(),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => PageBloc()),
+          BlocProvider(create: (_) => ThemeBloc())
+        ],
+        child: BlocBuilder<ThemeBloc, ThemeState>(
+          builder: (_, themeState) => MaterialApp(
+            theme: themeState.themeData,
+            debugShowCheckedModeBanner: false,
+            home: Wrapper(),
+          ),
+        ),
+      );
   }
 }
