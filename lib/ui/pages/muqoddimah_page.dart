59eda751fb1d8ca5520df806ad3bb6cb2a995b03
mumuhkustino <mumuhkustino@gmail.com>
2020-06-25 14:39:55 +0700

    Update muqoddimah kitab button in muqoddimah page

--- lib/ui/pages/muqoddimah_page.dart
+++ lib/ui/pages/muqoddimah_page.dart
@@ -78,7 +78,7 @@ class _MuqoddimahPageState extends State<MuqoddimahPage> {
                             ),
                             color: colorLightBlue,
                             onPressed: () {
-//                            context.bloc<PageBloc>().add(GoToMainPage());
+                            context.bloc<PageBloc>().add(GoToMuqoddimahKitabPage());
                             }),
                       ),
                       Container(
