cdb220b783c316b77c4b1582a1cfa30236aa9de1
mumuhkustino <mumuhkustino@gmail.com>
2020-06-26 16:26:43 +0700

    Add class entry for item in materi thaharah wudu page

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -69,4 +69,40 @@ TextSpan searchFromText(String fullText, String searchText) {
       ]);
 }
 
+class Entry {
+  Entry(this.title, this.content);
+
+  final String title;
+  final String content;
+}
+
+//class EntryItem extends StatefulWidget {
+//
+//  const EntryItem(this.entry);
+//
+//  final Entry entry;
+//
+//  @override
+//  _EntryItemState createState() => _EntryItemState();
+//}
+//
+//class _EntryItemState extends State<EntryItem> {
+//
+//  Widget _buildTiles(Entry root) {
+//    if (root.children.isEmpty) return ListTile(title: Text(root.title), onLongPress: root.title,);
+//    return
+//      ExpansionTile(
+//      key: PageStorageKey<Entry>(root),
+//      title: Text(root.title),
+//      children: root.children.map(_buildTiles).toList(),
+//    );
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return _buildTiles(widget.entry);
+//  }
+//}
+
+
 //file ini berisikan objek2 yang menyimpan data
