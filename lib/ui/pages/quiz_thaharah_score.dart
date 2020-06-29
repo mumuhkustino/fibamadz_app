65f10cab898066b0653e2790c94681922b98d156
mumuhkustino <mumuhkustino@gmail.com>
2020-06-29 20:36:37 +0700

    Update layout for quiz thaharah page and score

--- lib/ui/pages/quiz_thaharah_score.dart
+++ lib/ui/pages/quiz_thaharah_score.dart
@@ -1,7 +1,6 @@
 part of 'pages.dart';
 
 class QuizThaharahScore extends StatefulWidget {
-
   @override
   _QuizThaharahScoreState createState() => _QuizThaharahScoreState();
 }
@@ -19,58 +18,33 @@ class _QuizThaharahScoreState extends State<QuizThaharahScore> {
       },
       child: Scaffold(
         backgroundColor: colorCream,
-        body: Container(
-          child: ListView(
-            children: <Widget>[
-              Column(
+        body: Column(
+          children: [
+            // BUTTON PREVIOUS
+            Container(
+              margin: EdgeInsets.only(top: 20, bottom: 22),
+              height: 56,
+            ),
+            // END OF BUTTON PREVIOUS
+            Center(
+              child: Text(
+                "Quiz",
+                style: blackTextFont.copyWith(
+                    fontSize: 24, fontWeight: FontWeight.w700),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Thaharah",
+                style: blackTextFont.copyWith(fontSize: 14),
+              ),
+            ),
+            // TITLE DAN SUBTITLE
+            Expanded(
+              child: ListView(
                 children: [
-                  // BUTTON PREVIOUS
-                  Container(
-                    margin: EdgeInsets.only(top: 20),
-                    height: 56,
-                    child: Row(
-                      children: <Widget>[
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: GestureDetector(
-                            onTap: () {
-                              if (!isClickedSearch) {
-                                context.bloc<PageBloc>().add(GoToQuizPage());
-                              } else {
-                                setState(() {});
-                              }
-                            },
-                            child: Container(
-                                width: MediaQuery.of(context).size.width / 8,
-                                child: Icon(
-                                    (!isClickedSearch)
-                                        ? Icons.arrow_back_ios
-                                        : Icons.clear,
-                                    color: Colors.black.withOpacity(0.54))),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  // END OF BUTTON PREVIOUS
-
-                  // TITLE DAN SUBTITLE
                   Column(
                     children: [
-                      Center(
-                        child: Text(
-                          "Quiz",
-                          style: blackTextFont.copyWith(
-                              fontSize: 24, fontWeight: FontWeight.w700),
-                        ),
-                      ),
-                      Center(
-                        child: Text(
-                          "Thaharah",
-                          style: blackTextFont.copyWith(fontSize: 14),
-                        ),
-                      ),
-
                       // SCORE TEXT
                       Container(
                         margin: EdgeInsets.only(top: 72),
@@ -81,65 +55,61 @@ class _QuizThaharahScoreState extends State<QuizThaharahScore> {
                           ),
                         ),
                       ),
-                    ],
-                  ),
-
-                  //TULISAN SCORE DAN LINGKARAN HIJAU
-                  Stack(
-                    children: <Widget>[
-                      Container(
-                        height: 160,
-                        margin: EdgeInsets.only(top: 40),
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image:
-                                AssetImage("assets/icons/Ellipse-$score.png"),
+                      //TULISAN SCORE DAN LINGKARAN HIJAU
+                      Stack(
+                        children: <Widget>[
+                          Container(
+                            height: 160,
+                            margin: EdgeInsets.only(top: 40),
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image:
+                                    AssetImage("assets/icons/Ellipse-$score.png"),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      Container(
-                        alignment: Alignment.center,
-                        margin: EdgeInsets.only(top: 100),
-                        child: Text(
-                          '$score',
-                          style: blackTextFont.copyWith(
-                              fontSize: 24, fontWeight: FontWeight.w700),
-                        ),
-                      )
-                    ],
-                  ),
-                  
-                  // BUTTON BACK TO MENU
-                  Container(
-                    width: 200,
-                    height: 36,
-                    margin: EdgeInsets.only(top: 150),
-                    child: RaisedButton(
-                      elevation: 4,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Back to Menu",
-                            style: whiteTextFont.copyWith(fontSize: 14),
-                          ),
+                          Container(
+                            alignment: Alignment.center,
+                            margin: EdgeInsets.only(top: 100),
+                            child: Text(
+                              '$score',
+                              style: blackTextFont.copyWith(
+                                  fontSize: 24, fontWeight: FontWeight.w700),
+                            ),
+                          )
                         ],
                       ),
-                      color: colorDarkGrey,
-                      onPressed: () {
-                        context.bloc<PageBloc>().add(GoToMainPage());
-                      },
-                    ),
+
+                      // BUTTON BACK TO MENU
+                      Container(
+                        width: 200,
+                        height: 36,
+                        margin: EdgeInsets.only(top: 150),
+                        child: RaisedButton(
+                          elevation: 4,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(6)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Back to Menu",
+                                style: whiteTextFont.copyWith(fontSize: 14),
+                              ),
+                            ],
+                          ),
+                          color: colorDarkGrey,
+                          onPressed: () {
+                            context.bloc<PageBloc>().add(GoToMainPage());
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-              Column(
-                children: <Widget>[],
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
